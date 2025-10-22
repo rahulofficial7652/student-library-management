5c06f5a4cfb551d65d01dd2e8e44111c358eb6b1
Rahul <rahulofficial7652@gmail.com>
2025-10-22 22:45:18 +0530

    setup-project

---
+++ src/test/java/student_library_management/library_management/LibraryManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package student_library_management.library_management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
