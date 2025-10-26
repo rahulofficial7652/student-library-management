528885210693970f9601cd8b7b8578713a522c83
Rahul <rahulofficial7652@gmail.com>
2025-10-26 20:39:33 +0530

    Student add api created

--- src/main/java/student_library_management/library_management/repository/StudentRepository.java
+++ src/main/java/student_library_management/library_management/repository/StudentRepository.java
@@ -2,7 +2,6 @@ package student_library_management.library_management.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import student_library_management.library_management.model.Card;
 import student_library_management.library_management.model.Student;
 
 @Repository
